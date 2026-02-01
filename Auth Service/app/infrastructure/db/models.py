89503a7813f818b56dc07274e94fb4595f2bc5b7
perfecrtr <perfecrtr@gmail.com>
2026-02-01 19:49:22 +0300

    added user model for database

---
+++ Auth Service/app/infrastructure/db/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+
+from app.infrastructure.db.database import Base
+
+class UserModel(Base):
+    """User model in database"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True,index=True)
+    full_name = Column(String, nullable=True)
+    phone_number = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    last_login = Column(DateTime(timezone=True), nullable=True)
