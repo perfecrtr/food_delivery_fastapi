e175ced8ae0f087e572791b1e641747ad40e002a
perfecrtr <perfecrtr@gmail.com>
2026-02-01 16:00:24 +0300

    added config module for auth service

---
+++ Auth Service/app/core/config.py
@@ -0,0 +1,19 @@
+"""
+    Config module for Auth Service
+"""
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = 'Auth Service'
+
+    db_host: str
+    db_user: str
+    db_port: int
+    db_pass: str
+    db_name: str
+
+    jwt_secret_key: str
+    jwt_algorithm: str
+    accesss_token_expire_minutes: int
+    refresh_token_expire_days: int
