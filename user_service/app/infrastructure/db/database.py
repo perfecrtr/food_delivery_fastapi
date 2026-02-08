ca6c8a87feffa3517eec8a9b22475a0df7b93d1b
perfecrtr <perfecrtr@gmail.com>
2026-02-08 15:08:42 +0300

    added Database Configuration module for User Service

--- user_service/app/infrastructure/db/database.py
+++ user_service/app/infrastructure/db/database.py
@@ -1,7 +1,35 @@
+"""
+    Database Configuration module for User Service
+"""
+
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Null
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from app.core.config import settings
 
 class Base(DeclarativeBase):
     type_annotation_map = {
         None: Null
     }
+
+engine = create_async_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+    echo=False,
+    future=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.aclose()
