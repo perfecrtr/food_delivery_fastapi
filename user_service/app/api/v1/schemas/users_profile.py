eb974515480da156909fcb551c66c2ed04884f0d
perfecrtr <perfecrtr@gmail.com>
2026-02-08 16:20:16 +0300

    added schemas for User Service

---
+++ user_service/app/api/v1/schemas/users_profile.py
@@ -0,0 +1,33 @@
+"""
+    API schemas for user profile endpoints
+"""
+
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+from app.domain.enums import Gender
+
+class CreateUserProfileRequest(BaseModel):
+    """Request schema for user profile creating"""
+    id: int
+    phone_number: str
+    full_name: str
+
+class CreateUserProfileResponse(BaseModel):
+    id: int
+    msg: str
+
+class GetUserProfileRequest(BaseModel):
+    id: int
+
+class GetUserProfileResponse(BaseModel):
+    id: int
+    phone_number: str
+    full_name: str
+    email: Optional[str]
+    address: Optional[str]
+    birthday_date: Optional[date]
+    gender: Optional[Gender]
+
+
+
