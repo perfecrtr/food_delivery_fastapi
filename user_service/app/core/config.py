c28e67a62fbef24320ec617651bcf081daa7d431
perfecrtr <perfecrtr@gmail.com>
2026-02-08 23:20:39 +0300

    added kafka settings for setting in User Service config

--- user_service/app/core/config.py
+++ user_service/app/core/config.py
@@ -14,6 +14,17 @@ class Settings(BaseSettings):
     db_port: int = Field(..., env="DB_PORT")
     db_pass: str = Field(..., env="DB_PASS")
     db_name: str = Field(..., env="DB_NAME")
+
+    KAFKA_BOOTSTRAP_SERVERS: str 
+    KAFKA_CLIENT_ID: str
+    
+    KAFKA_PRODUCER_ACKS: str
+    KAFKA_PRODUCER_RETRIES: int
+    KAFKA_PRODUCER_MAX_BLOCK_MS: int
+    KAFKA_PRODUCER_COMPRESSION_TYPE: str
+    KAFKA_CONSUMER_GROUP: str
+    
+    KAFKA_TOPIC_USER_REGISTERED: str 
     
     class Config:
         env_file = ".env"
