c0bad9c6e89d56d6b6c338d3e99830304c6fe59f
perfecrtr <perfecrtr@gmail.com>
2026-02-08 18:41:17 +0300

    added router in main.py

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from app.api.v1.endpoints.users import router as user_router
 
 app = FastAPI()
-
+app.include_router(user_router, prefix="/api/v1/user")
 
 @app.get("/")
 async def root():
