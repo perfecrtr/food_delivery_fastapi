e87ac25c703da46e7ec169b8177c7d60a84a602d
perfecrtr <perfecrtr@gmail.com>
2026-02-08 19:11:05 +0300

    added dependency

--- user_service/app/core/dependencies.py
+++ user_service/app/core/dependencies.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 
 from app.application.commands.create_user_profile import CreateUserProfileHandler
+from app.application.queries.get_user_profile import GetUserProfileHandler
 from app.infrastructure.db.database import get_db
 from app.infrastructure.db.repository import UserProfileRepository
 
@@ -17,5 +18,12 @@ async def get_creating_profile_handler(user_profile_repository = Depends(get_use
     return(
         CreateUserProfileHandler(
         user_profile_repository
+        )
+    )
+
+async def get_get_user_profile_handler(user_profile_repository = Depends(get_user_profile_repository)):
+    return(
+        GetUserProfileHandler(
+            user_profile_repository
         )
     )
