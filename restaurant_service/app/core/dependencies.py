8d7ba3ed43ecfe0e4a7be2a56290dc6d4667cf0c
perfecrtr <perfecrtr@gmail.com>
2026-02-09 15:58:53 +0300

    small fix

--- restaurant_service/app/core/dependencies.py
+++ restaurant_service/app/core/dependencies.py
@@ -8,20 +8,20 @@ from app.infrastructure.db.repository import RestaurantRepository
 
 
 
-async def get_user_profile_repository(db: AsyncSession = Depends(get_db)) -> RestaurantRepository:
+async def get_restaurant_repository(db: AsyncSession = Depends(get_db)) -> RestaurantRepository:
     """
     Get user repository instance
     """
     return RestaurantRepository(db)
 
-async def get_creating_restaurant_handler(user_profile_repository = Depends(get_user_profile_repository)):
+async def get_creating_restaurant_handler(user_profile_repository = Depends(get_restaurant_repository)):
     return(
         CreateRestaurantHandler(
         user_profile_repository
         )
     )
 
-async def get_getting_all_restaurants_handler(user_profile_repository = Depends(get_user_profile_repository)):
+async def get_getting_all_restaurants_handler(user_profile_repository = Depends(get_restaurant_repository)):
     return(
         GetAllRestrauntsHandler(
         user_profile_repository
