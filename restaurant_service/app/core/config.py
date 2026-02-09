092763f58daae27e0d368eff89591f98c9674d40
perfecrtr <perfecrtr@gmail.com>
2026-02-09 13:00:38 +0300

    added config module for Restaurant Service

---
+++ restaurant_service/app/core/config.py
@@ -0,0 +1,28 @@
+"""
+    Config module for Restaurant Service
+"""
+
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+class Settings(BaseSettings):
+    app_name: str = 'Restaurant Service'
+
+    db_host: str = Field(..., env="DB_HOST")
+    db_user: str = Field(..., env="DB_USER")
+    db_port: int = Field(..., env="DB_PORT")
+    db_pass: str = Field(..., env="DB_PASS")
+    db_name: str = Field(..., env="DB_NAME")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = False
+        extra = "ignore"
+
+    @property
+    def database_url(self) -> str:
+        """Return full database URL"""
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+settings = Settings()
