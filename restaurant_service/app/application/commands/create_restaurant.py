8103e28b935af0b2489fd76c267768bd5cf7ff00
perfecrtr <perfecrtr@gmail.com>
2026-02-09 15:55:09 +0300

    added comand and handler for creating restaurant in Restaurant Service

---
+++ restaurant_service/app/application/commands/create_restaurant.py
@@ -0,0 +1,50 @@
+"""
+    Create Restaurant Command and Handler
+"""
+
+from dataclasses import dataclass
+from typing import Optional
+from uuid import uuid4
+from datetime import datetime
+from app.domain.entities.restaurant import Restaurant
+from app.infrastructure.db.repository import RestaurantRepository
+
+@dataclass
+class CreateRestaurantCommand:
+    name: str
+    address: str
+    contact_phone: str
+    opening_hours: dict
+    is_active: Optional[bool] = None
+    description: Optional[str] = None
+    coordinates: Optional[dict] = None
+    tags: Optional[list] = None
+
+class CreateRestaurantHandler:
+
+    def __init__(
+        self,
+        restaurant_repository: RestaurantRepository
+    ):
+        self.restaurant_repository = restaurant_repository
+
+    async def handle(self, command: CreateRestaurantCommand) -> dict:
+
+        restaurant = Restaurant(
+            id=uuid4(),
+            name=command.name,
+            address=command.address,
+            contact_phone=command.contact_phone,
+            opening_hours=command.opening_hours,
+            is_active=command.is_active,
+            description=command.description,
+            coordinates=command.coordinates,
+            tags=command.tags
+        )
+
+        saved_restaurant_profile = await self.restaurant_repository.create_restaurant(restaurant)
+
+        return {
+            "id": saved_restaurant_profile.id,
+            "msg": "Restaurant successfully created!"
+        }
