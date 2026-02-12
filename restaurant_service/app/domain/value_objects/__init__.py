34bf396086951666dbb7ab997f481eef116505af
perfecrtr <perfecrtr@gmail.com>
2026-02-12 17:13:58 +0300

    small fix

--- restaurant_service/app/domain/value_objects/__init__.py
+++ restaurant_service/app/domain/value_objects/__init__.py
@@ -1,3 +1,2 @@
 from app.domain.value_objects.address import Address
 from app.domain.value_objects.phone_number import PhoneNumber
-from app.domain.value_objects.working_schedule import WorkingSchedule
